45a8630674d333ec093a8b63590da9779c22c8c7
Dominica Phetteplace <wicked_light_sleeper@yahoo.com>
2015-03-01 18:50:15 -0800

    added comments

--- diffe.py
+++ diffe.py
@@ -1,6 +1,10 @@
+#this will print out a list of triples
+#the last component will be your slope
+
+
 def dy_dx(x,y):
     return float(x*y/2)
 
 for x in range(-3,4):
     for y in range(-3,4):
-        print (x, y, dy_dx(x,1.0))    
+        print (x, y, dy_dx(x,y))    
